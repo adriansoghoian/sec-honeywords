6c6d56756893988140964b879d96ebfacc923c5d
andywow <andywang@dhcp-ccc-8481.redrover.cornell.edu>
2014-10-23 16:38:56 -0400

    reconstruct honeyword

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from letters import *
 from number import *
 from symbol_split import *
 
+totalHoneywordNum = 19
+
 def split_password(string):
 	password_indices = []
 	output = []
@@ -31,9 +33,28 @@ def generate_variations(substring):
 		return generate_strings(substring)
 
 def reconstruct_honeyword(variations, password_indices):
-	for i, char_type in enumerate(password_indices)
+ 
+    start_indices = []
+    for i, char_type in enumerate(password_indices):
+        for j, substring in enumerate(char_type):
+            start_indices.append(int(substring[0]))
+    start_indices.sort()
 
-
+    priority_indices = copy.copy(start_indices)
+    for i, char_type in enumerate(password_indices):
+        for j, substring in enumerate(char_type):
+            index = start_indices.index(substring[0])
+            priority_indices[index] = (i,j)
+    
+    output = []
+    for times in range(0,totalHoneywordNum):
+        psw = ''
+        for pos in priority_indices:
+            k = randrange(0,len(variations[pos[0]][pos[1]]))
+            randString = variations[pos[0]][pos[1]][k]
+            psw = psw + randString
+        output.append(psw)
+    return output
 
 if __name__ == "__main__":
 	if len(sys.argv) == 2:
@@ -42,3 +63,6 @@ if __name__ == "__main__":
 		print "Please pass in a password as a command line argument, please."
 	variations, password_indices = split_password(password)
 	print password_indices
+
+	honeyword = reconstruct_honeyword(variations, password_indices)
+	print honeyword
