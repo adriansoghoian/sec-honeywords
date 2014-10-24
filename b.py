2cb1cac438fcfee9389b66ff7beab3f1ae96c296
adriansoghoian <adriansoghoian@gmail.com>
2014-10-24 13:52:04 -0400

    Added files and pseudocode for next 2 problems

---
+++ b.py
@@ -0,0 +1,11 @@
+from a import *
+
+if __name__ == "__main__":
+	## Passes in the password they want and the number of output passwords (n)
+	## N = number of desired output passwords 
+	## Pick 2 passwords individually from RockYou. 
+	## Pick another two and concatenate them
+	## Generate one toughnut
+	## Scramble all 5 passwords
+	## Put back together in a n-length list
+
---
+++ c.py
@@ -0,0 +1,3 @@
+## Calculate top 4 closest passwords in rockyou using the text similarity library
+## Scramble these 4 along with the original passwords
+## Output n strings (depending on the user input)
